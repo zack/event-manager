8697c2f22116326faf875f0d100dd839045ffabe
Zack Youngren <zack@youngren.io>
2019-07-02 20:22:52 -0400

    Finished admin login + small style updates to tables

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,7 @@
 class AdminController < ApplicationController
+  before_action :validate_session, except: [:login, :process_login]
+
   def index
-    if not session[:admin]
-      redirect_to :admin_login
-    end
   end
 
   def login
@@ -10,12 +9,14 @@ class AdminController < ApplicationController
 
   def logout
     session[:admin] = nil
-    redirect_to :admin_index
+    flash[:success] = 'You have successfully logged out'
+    redirect_to :admin_login
   end
 
   def process_login
     if params[:password] === ENV.fetch('ADMIN_PASSWORD')
       session[:admin] = Time.now.to_i
+      flash[:success] = 'You have successfully logged in'
       redirect_to :admin_index
     else
       flash[:warning] = 'Bad Password'
@@ -26,4 +27,13 @@ class AdminController < ApplicationController
   def users
     @users = User.all
   end
+
+  private
+
+    def validate_session
+      if not session[:admin]
+        flash[:warning] = 'You must be logged in'
+        redirect_to :admin_login
+      end
+    end
 end
