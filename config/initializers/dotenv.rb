a7c7c5b03625924763994f8671f9324c88aef271
Zack Youngren <zack@youngren.io>
2019-08-26 23:41:05 -0400

    prevent dotenv from crashing production

--- config/initializers/dotenv.rb
+++ config/initializers/dotenv.rb
@@ -1,7 +1,10 @@
-Dotenv.require_keys(
-  'MAILGUN_API_KEY',
-  'MAILING_LIST_NAME',
-  'EMAIL_USER',
-  'EMAIL_DOMAIN',
-  'ADMIN_PASSWORD'
-)
+Bundler.require(*Rails.groups)
+if ['development', 'test'].include? ENV['RAILS_ENV']
+  Dotenv.require_keys(
+    'MAILGUN_API_KEY',
+    'MAILING_LIST_NAME',
+    'EMAIL_USER',
+    'EMAIL_DOMAIN',
+    'ADMIN_PASSWORD'
+  )
+end
