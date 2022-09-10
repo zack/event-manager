703cf0d96aa86949ed5959c98afe45b8b8ad1c17
Zack Youngren <zack@youngren.io>
2022-09-10 18:43:16 -0400

    show events on admin index that haven't ended yet

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,7 @@ class AdminController < ApplicationController
   before_action :require_admin_login, except: [:login, :process_login]
 
   def index
-    @events = Event.where('datetime > ?', DateTime.now).order(datetime: :asc)
+    @events = Event.where('datetime_end > ?', DateTime.now).order(datetime: :asc)
   end
 
   def test_exception
