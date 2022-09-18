f984ab629bb50c7f121d1763048b4aff8c69ea8a
Zack Youngren <zack@youngren.io>
2022-09-18 17:04:59 -0400

    lots of good work on special events
    * schema and models for special events and special event syndications
    * controller for special events
    * templates for special event index, admin, create
    
    to do:
    * figure out how to add/invite people
    * add 'edit' ability
    * test everything

---
+++ app/controllers/special_events_controller.rb
@@ -0,0 +1,163 @@
+class SpecialEventsController < ApplicationController
+  before_action :require_admin_login, only: [
+    :index, :new, :create, :admin, :edit, :update, :delete, :destroy, :syndicate, :invite_user
+  ]
+
+  def index
+    @special_events = SpecialEvent.all.order(datetime: :desc)
+    @upcoming_special_events = @special_events.select { |e| (e.datetime_end > DateTime.now) && (not e.deleted) }
+    @deleted_special_events = @special_events.select { |e| e.deleted }
+    @past_special_events = @special_events.select { |e| (e.datetime < DateTime.now) && !e.deleted }
+  end
+
+  def new
+    @special_event = SpecialEvent.new
+  end
+
+  def create
+    @special_event = SpecialEvent.new(event_params)
+    @special_event.uuid = SecureRandom.uuid
+    @special_event.deleted = false
+
+    if @special_event.save
+      redirect_to action: :admin, uuid: @special_event.uuid
+    else
+      @special_event.uuid = nil
+      render :new
+    end
+  end
+
+  def admin
+    @special_event = SpecialEvent.find_by uuid: params['uuid']
+    @invited = SpecialEventSyndication.where(special_event_id: @special_event)
+
+    if @special_event.deleted
+      flash[:warning] = 'This event has been soft deleted.'
+    end
+  end
+
+  def edit
+    @special_event = SpecialEvent.find_by uuid: params['uuid']
+  end
+
+  # def update
+  #   @special_event = SpecialEvent.find_by uuid: params['event']['uuid']
+
+  #   old_event = @special_event.attributes
+  #   @special_event.assign_attributes(event_params)
+  #   event_changed = @special_event.changed?
+
+  #   if @special_event.save
+  #     if event_changed && params[:suppress_email] == '1'
+  #       flash[:success] = 'SpecialEvent successfully updated! Update email suppressed.'
+  #     elsif event_changed
+  #       users = User.includes(:syndications).where(syndications: { event_id: @special_event })
+  #       users.each do |u|
+  #         UserMailer.event_updated(u, @special_event, old_event).deliver_later
+  #       end
+  #       user_string = ActionController::Base.helpers.pluralize(users.count, 'user')
+  #       flash[:success] = "SpecialEvent successfully updated! Notified #{user_string}."
+  #     else
+  #       flash[:notice] = 'No changes detected.'
+  #     end
+  #     redirect_to action: :edit
+  #   else
+  #     render :edit
+  #   end
+  # end
+
+  def check_past_or_deleted(special_event)
+    if special_event.deleted
+      render :deleted
+      return
+    end
+
+    if special_event.datetime_end < DateTime.now
+      render :past
+    end
+  end
+
+  def submit_rsvp
+    special_event = SpecialEvent.find_by uuid: params[:uuid]
+    invitee = SpecialEventSyndication.find_by email_address: params[:email_address]
+    invitee.update(rsvp: params[:rsvp])
+
+    flash[:success] = 'RSVP Updated'
+    redirect_to action: :admin, special_event: params[:uuid]
+  end
+
+  def delete
+    @special_event = SpecialEvent.find_by uuid: params[:uuid]
+  end
+
+  def soft_delete
+    @special_event = SpecialEvent.find_by uuid: params['uuid']
+    @special_event.update(deleted: true)
+
+    invitees = SpecialEventSyndications.where({ event_id: @special_event })
+    reason = params['reason']
+    invitees.each do |i|
+      UserMailer.special_event_deleted(i, @special_event, reason).deliver_later
+    end
+
+    invitee_string = ActionController::Base.helpers.pluralize(invitees.count, 'invitee')
+    flash[:success] = "Special event successfully soft deleted! Notified #{user_string}."
+    redirect_to action: :index
+  end
+
+  # def syndicate_preview
+  #   @special_event = SpecialEvent.find_by uuid: params['uuid']
+  #   @address = Address.find(@special_event.address_id)
+  #   @users = get_users_for_event_syndication(@special_event)
+  # end
+
+  # def syndicate
+  #   @special_event = SpecialEvent.find_by uuid: params['uuid']
+  #   @users = get_users_for_event_syndication(@special_event)
+  #   @users.each do |u|
+  #     UserMailer.invite(u, @special_event).deliver_later
+  #     Syndication.create(
+  #       event_id: @special_event.id,
+  #       user_id: u.id
+  #     )
+  #   end
+
+  #   flash[:succes] = "Successfully invited #{@users.count} users!"
+  #   redirect_to action: :admin, uuid: @special_event.uuid
+  # end
+
+  # def invite_user
+  #   @user = User.find(params['user_id'])
+  #   @special_event = SpecialEvent.find(params['event_id'])
+  #   UserMailer.invite(@user, @special_event).deliver_now
+  #   Syndication.create(event_id: @special_event.id, user_id: @user.id)
+  #   redirect_to action: :admin, uuid: @special_event.uuid
+  # end
+
+  private
+
+    def get_users_for_event_syndication(event)
+      users = User
+        .includes(:subscription_lists)
+        .includes(:syndications)
+        .where(users: { email_confirmed: true })
+        .where(users: { admin_confirmed: true })
+        .where(subscription_lists: { id: @special_event.subscription_list_id })
+
+      # there's probably a way to do this in the above query, but this works
+      @users = users.filter do |u|
+        Syndication.where(user_id: u, event_id: @special_event).count == 0
+      end
+    end
+
+    def event_params
+      params.require(:special_event).permit(
+        :address_id,
+        :capacity,
+        :datetime,
+        :datetime_end,
+        :description,
+        :uuid
+      )
+    end
+end
---
+++ app/models/special_event.rb
@@ -0,0 +1,66 @@
+class SpecialEvent < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
+  has_one :address
+
+  has_many :special_event_syndications
+
+  validate :check_attendance_below_limit
+  validate :check_end_after_start
+  validates :datetime, presence: true, uniqueness: true
+  validates :datetime_end, presence: true
+  validates :description, presence: true
+
+  def attendees
+    SpecialEventSyndication.where(special_event_id: id).where('rsvp > ?', 0).pluck(:rsvp).reduce(:+) || 0
+  end
+
+  def maybes
+    SpecialEventSyndication.where(special_event_id: id).where(rsvp: 0).count
+  end
+
+  def create_ics(attendee)
+    rsvp_url = event_rsvp_path(uuid, attendee.uuid)
+
+    ics_description =
+    <<~EOS
+      RSVP Here: https://#{ENV.fetch('HOST')}#{rsvp_url}
+
+      #{description}
+    EOS
+
+    cal = Icalendar::Calendar.new
+    cal.event do |e|
+      e.dtstart     = datetime.utc.strftime('%Y%m%dT%H%M%SZ')
+      e.dtend       = datetime_end.utc.strftime('%Y%m%dT%H%M%SZ')
+      e.attendee    = "mailto:#{attendee.email_address}"
+      e.summary     = subscription_list.name
+      e.location    = Address.find(address_id).formatted_full_one_line
+      e.organizer   = "mailto:#{ENV.fetch('EVENT_ORGANIZER_USER')}@#{ENV.fetch('EMAIL_DOMAIN')}"
+      e.description = ics_description
+    end
+
+    # Using 'REQUEST' means that when the recipient selects a response it will
+    # get emailed back to the organizer email address. Make sure that address
+    # can receive mail or else the user will get a bounceback. If you don't
+    # want this behavior, you can change the method to 'PUBLISH'. Email clients
+    # will add an "Add this event to your calendar" button instead of
+    # "Yes/No/Maybe" RSVP buttons
+    cal.ip_method = 'PUBLISH'
+    cal.to_ical
+  end
+
+  private
+
+    def check_attendance_below_limit
+      if capacity && attendees >= capacity
+        errors.add(:capacity, 'has been reached!')
+      end
+    end
+
+    def check_end_after_start
+      if datetime_end < datetime
+        errors.add(:datetime_end, 'is before start')
+      end
+    end
+end
