6c06a3a54771f1e1d36bca61002c414b1f543457
Zack Youngren <zack@youngren.io>
2022-09-19 20:07:06 -0400

    Add inviting for special event guests guests (no email yet)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_18_195312) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_19_221948) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_18_195312) do
     t.integer "rsvp"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "invited", null: false
     t.index ["special_event_id"], name: "index_special_event_syndications_on_special_event_id"
   end
 
